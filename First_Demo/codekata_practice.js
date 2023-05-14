ed5e981cda8614fcd05d45104baa3b347a70da66
IndiraRamalingam <128180147+IndiraRamalingam@users.noreply.github.com>
2023-05-14 19:57:48 +0530

    First_Codekata_Practice

---
+++ First_Demo/codekata_practice.js
@@ -0,0 +1,10 @@
+let string="sundar"
+let subString="sun"
+if(string.includes(subString))
+{
+    console.log("yes")
+}
+else{
+    console.log("no")
+}
+https://www.geeksforgeeks.org/binary-search-in-javascript/
---
+++ First_Demo/first.js
@@ -0,0 +1,51 @@
+let resume_Data =
+{
+    Summary:
+    {
+     Summary:"Around 3 Years of Experience in IT industry. Having experience in AGILE methodology. Fast learner with Dynamic Nature"   
+    },
+    Education:
+        {
+        Qualification:"Bachelar Of Engineering(CSE)",
+        Year_Of_Passing:"2015",
+        },
+    Technical_Skills:
+    {
+        Operating_System:"Windows",
+        Languages:["Java","Javascript"],
+        Technologies:["HTML","CSS"],
+        IDE:["Eclipse","VScode"],
+        CMS:["Jahia","Evoq"]
+        
+    },
+    Work_History:
+    {
+        Role:"Programmer Analyst",
+        Company:"Excelacom Technologies",
+        Period:"September 2015 to Febraury 2019"
+    },
+    Professional_Experience:
+    [ 
+        {
+            Project:"NorthStar Retail Portal",
+            Role:"Programmer Analyst",
+            Duration:"Jan 2017 to Feb 2019",
+            Project_Description:"FairPoint website will allow our largest business and public sector customers to use the web for the following self-help capabilities. FairPoint services include local and long distance phone service, data, Internet, broadband, television, business communications solutions and fiber services based on service location."
+        },
+        {
+            Project:"Firefly - Customer Manager",
+            Role:"Programmer Analyst",
+            Duration:"Dec 2015 to Jan 2017",
+            Project_Description:"Century application provide telecommunication service providers with a ready-to-use ordering solution, allowing them to accelerate the introduction of new products, pursue new revenue opportunities, deploy product enhancements, improve order completion, and improve the customer experience."
+        }
+    ],
+    First_Name:"Indira",
+    Last_Name:"Ramalingam",
+    Date_Of_Birth:"28 Nov 1993",
+    Mobile_Number:"8888888888"
+    
+}
+console.log(resume_Data);
+
+
+
